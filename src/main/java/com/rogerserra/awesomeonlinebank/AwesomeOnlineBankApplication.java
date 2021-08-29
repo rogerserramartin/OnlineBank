b22155eae2cfb651215f4d09a03b124813b91ba4
rogersm92 <roger.serra92@gmail.com>
2021-08-29 12:52:55 +0200

    Springboot dependencies

---
+++ src/main/java/com/rogerserra/awesomeonlinebank/AwesomeOnlineBankApplication.java
@@ -0,0 +1,13 @@
+package com.rogerserra.awesomeonlinebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeOnlineBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeOnlineBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogerserra/awesomeonlinebank/AwesomeOnlineBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerserra.awesomeonlinebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeOnlineBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
